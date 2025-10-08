e478a9bbdc0e9b4e4ca7780f179943d6417f6720
sewakj27 <sewakj27@greenhill.org>
2025-10-08 16:21:40 -0500

    Fixed DriverTestTeleOp and TeleOpTemplate motor directions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/DriverTestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/DriverTestTeleOp.java
@@ -49,7 +49,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Driver Test TeleOp", group="Decode")
+@TeleOp(name="Driver Test TeleOp", group="A_Decode")
 
 public class DriverTestTeleOp extends OpMode
 {
