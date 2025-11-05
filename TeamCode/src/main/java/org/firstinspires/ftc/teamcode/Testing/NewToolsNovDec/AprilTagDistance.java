e631061c2330295f4e90b4b484250d0858737396
TKL-Torre <delatorreg27@greenhill.org>
2025-11-05 14:33:50 -0600

    April Tag Stuff Tool Class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/NewToolsNovDec/AprilTagDistance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/NewToolsNovDec/AprilTagDistance.java
@@ -29,12 +29,16 @@
 
 package org.firstinspires.ftc.teamcode.Testing.NewToolsNovDec;
 
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.AutonAssets.drive.PatternStorage;
+import org.firstinspires.ftc.teamcode.Testing.Location_Gabe_Johnny_Sammie.AprilTagToolClassSingleVision;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -54,46 +58,15 @@ import java.util.List;
 
 public class AprilTagDistance extends OpMode
 {
-    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
-    private static final boolean isBlue = false;
-    /**
-     * The variable to store our instance of the AprilTag processor.
-     */
-    private AprilTagProcessor aprilTag;
 
-    /**
-     * The variable to store our instance of the vision portal.
-     */
-    private VisionPortal visionPortal;
+    private AprilTagToolClass aprilTagToolClass;
+    private ElapsedTime runtime = new ElapsedTime();
     double distance = 6.7;
 
-
-    private ElapsedTime runtime = new ElapsedTime();
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
     @Override
     public void init() {
         telemetry.addData("Status", "Initializing");
-        aprilTag = new AprilTagProcessor.Builder().build();
-
-
-        aprilTag.setDecimation(1);
-
-        // Create the vision portal by using a builder.
-        VisionPortal.Builder builder = new VisionPortal.Builder();
-
-        // Set the camera (webcam vs. built-in RC phone camera).
-        if (USE_WEBCAM) {
-            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
-        } else {
-            builder.setCamera(BuiltinCameraDirection.BACK);
-        }
-
-        builder.addProcessor(aprilTag);
-
-        visionPortal = builder.build();
-
+        aprilTagToolClass = new AprilTagToolClass(hardwareMap, telemetry, gamepad1);
         telemetry.addData("Status", "Initialized");
     }
 
@@ -122,33 +95,11 @@ public class AprilTagDistance extends OpMode
     public void loop() {
 
         telemetry.addData("Status", "Run Time: " + runtime.toString());
-        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
-        telemetry.addData("# AprilTags Detected", currentDetections.size());
+        distance = aprilTagToolClass.getDistance();
+        ;
+        telemetry.addData("Distance", distance);
 
-        // Step through the list of detections and display info for each one.
-        for (AprilTagDetection detection : currentDetections) {
-            if (detection.metadata != null) {
-                if (detection.id == 20 && isBlue){
-                    distance = detection.ftcPose.range;
-                }
-                else if (detection.id == 24 && !isBlue){
-                    distance = detection.ftcPose.range;
-                }
-                else{
-                    distance = 100;
-                }
-                telemetry.addLine(String.format("Distance: " + distance));
 
-            } else {
-                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
-                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
-            }
-        }   // end for() loop
-
-        // Add "key" information to telemetry
-        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
-        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
-        telemetry.addLine("RBE = Range, Bearing & Elevation");
 
     }
 
