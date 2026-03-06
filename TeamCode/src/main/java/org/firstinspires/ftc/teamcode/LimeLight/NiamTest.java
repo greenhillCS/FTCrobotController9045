acbf4de43466631dcd355da8dc242febee3b0edb
ghelanin27 <GhellaniN27@greenhill.org>
2026-03-05 21:16:35 -0600

    hmmmmmmmmm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimeLight/NiamTest.java
@@ -0,0 +1,209 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.LimeLight;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
+import java.util.List;
+
+/*
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When a selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ */
+//TODO:Uncomment one of the following and rename group and name as needed.
+//@TeleOp(name="Change the name of your TeleOp", group="zzzzz")
+//@Autonomous(name="Change the name of your Auton", group="zzzzz")
+
+public class NiamTest extends OpMode {
+    // Declare OpMode members.
+    Limelight3A limelight;
+    double DESIRED_HEADING = 0;
+
+    //  Set the GAIN constants to control the relationship between the measured position error, and how much power is
+    //  applied to the drive motors to correct the error.
+    //  Drive = Error * Gain    Make these values smaller for smoother control, or larger for a more aggressive response.
+    final double SPEED_GAIN = 0.02;   //  Forward Speed Control "Gain". eg: Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
+    final double STRAFE_GAIN = 0.015;   //  Strafe Speed Control "Gain".  eg: Ramp up to 25% power at a 25 degree Yaw error.   (0.25 / 25.0)
+    final double TURN_GAIN = 0.01;   //  Turn Control "Gain".  eg: Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
+
+    final double MAX_AUTO_SPEED = 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
+    final double MAX_AUTO_STRAFE = 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
+    final double MAX_AUTO_TURN = 0.3;   //  Clip the turn speed to this max value (adjust for your robot)
+    private DcMotor frontLeft = null;  //  Used to control the left front drive wheel
+    private DcMotor frontRight = null;  //  Used to control the right front drive wheel
+    private DcMotor backLeft = null;  //  Used to control the left back drive wheel
+    private DcMotor backRight = null;  //  Used to control the right back drive wheel
+    private static final boolean USE_WEBCAM = true;  // Set true to use a webcam, or false for a phone camera
+    private static final int DESIRED_TAG_ID = 24;     // Choose the tag you want to approach or set to -1 for ANY tag.
+    private ElapsedTime runtime = new ElapsedTime();
+    boolean targetFound = false;    // Set to true when an AprilTag target is detected
+    double drive = 0;        // Desired forward power/speed (-1 to +1)
+    double strafe = 0;        // Desired strafe power/speed (-1 to +1)
+    double turn = 0;        // Desired turning power/speed (-1 to +1)
+             public void update(double DESIRED_DISTANCE, double DESIRED_YAW){
+
+                     LLResult result = limelight.getLatestResult();
+                     if (result != null && result.isValid()) {
+                         double tx = result.getTx(); // How far left or right the target is (degrees)
+                         double ty = result.getTy(); // How far up or down the target is (degrees)
+                         double ta = result.getTa(); // How big the target looks (0%-100% of the image)
+                         double yawError = 0;
+                         double headingError = 0;
+                         double rangeError = 0;
+                         List<LLResultTypes.FiducialResult> fiducials = result.getFiducialResults();
+                         for (LLResultTypes.FiducialResult fiducial : fiducials) {
+                             int id = fiducial.getFiducialId(); // The ID number of the fiducial
+                             double x = fiducial.getTargetXDegrees(); // Where it is (left-right)
+                             double y = fiducial.getTargetYDegrees(); // Where it is (up-down)
+                             Pose3D pose = fiducial.getTargetPoseCameraSpace();
+                             double range = pose.getPosition().z * 39.3701;
+                             double yaw = pose.getOrientation().getPitch(AngleUnit.DEGREES) * -1;
+                             double heading = tx;
+                             yawError = yaw - DESIRED_YAW;
+                             headingError = heading - DESIRED_HEADING;
+                             rangeError = range - DESIRED_DISTANCE;
+
+                             telemetry.addData("Range", range);
+                             telemetry.addData("Heading", heading);
+                             telemetry.addData("Yaw", yaw);
+
+                             telemetry.addData("Range Error", rangeError);
+                             telemetry.addData("Heading Error", headingError);
+                             telemetry.addData("Yaw Error", yawError);
+
+                         }
+                         // Use the speed and turn "gains" to calculate how we want the robot to move.
+                         drive = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
+                         turn = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN);
+                         strafe = Range.clip(-yawError * STRAFE_GAIN, -MAX_AUTO_STRAFE, MAX_AUTO_STRAFE);
+
+                     } else {
+                         drive = 0.0;
+                         strafe = 0.0;
+                         turn = 0.2;
+                         telemetry.addData("Limelight", "No Targets");
+                     }
+                     moveRobot(drive, strafe, turn);
+            }
+    public void moveRobot(double x, double y, double yaw) {
+        // Calculate wheel powers.
+        double leftFrontPower    =  x -y -yaw;
+        double rightFrontPower   =  x +y +yaw;
+        double leftBackPower     =  x +y -yaw;
+        double rightBackPower    =  x -y +yaw;
+
+        // Normalize wheel powers to be less than 1.0
+        double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+        max = Math.max(max, Math.abs(leftBackPower));
+        max = Math.max(max, Math.abs(rightBackPower));
+
+        if (max > 1.0) {
+            leftFrontPower /= max;
+            rightFrontPower /= max;
+            leftBackPower /= max;
+            rightBackPower /= max;
+        }
+
+        // Send powers to the wheels.
+        frontLeft.setPower(leftFrontPower);
+        frontRight.setPower(rightFrontPower);
+        backLeft.setPower(leftBackPower);
+        backRight.setPower(rightBackPower);
+    }
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        frontLeft = hardwareMap.get(DcMotor.class, "rightBack");
+        frontRight = hardwareMap.get(DcMotor.class, "leftBack");
+        backLeft = hardwareMap.get(DcMotor.class, "rightFront");
+        backRight = hardwareMap.get(DcMotor.class, "leftFront");
+
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        backLeft.setDirection(DcMotor.Direction.FORWARD);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+        Limelight3A limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
+        limelight.start(); // This tells Limelight to start looking!
+        limelight.pipelineSwitch(0); // Switch to pipeline number
+        telemetry.addData("Status", "Initializing");
+        telemetry.addData("Status", "Initialized");
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+
+        runtime.reset();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    NiamTest obj;
+    @Override
+    public void loop() {
+obj.update(120, -45);
+obj.update(30, -45);
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+}
+
+
