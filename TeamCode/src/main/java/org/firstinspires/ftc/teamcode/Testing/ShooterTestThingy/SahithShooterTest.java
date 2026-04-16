edad267bb92eb95380f28b28680609b2b1ae4e08
ghelanin27 <GhellaniN27@greenhill.org>
2026-04-16 16:11:28 -0500

    Ts should work (now it should work)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ShooterTestThingy/SahithShooterTest.java
@@ -0,0 +1,151 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.Testing.ShooterTestThingy;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoControllerEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Testing.AlianceColor.AlianceColorSyncTool;
+import org.firstinspires.ftc.teamcode.Tools.Intake;
+import org.firstinspires.ftc.teamcode.Tools.ManualTools;
+
+/*
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When a selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all iterative OpModes contain.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Sahith Shooter TeleOp", group="Testing")
+
+public class SahithShooterTest extends OpMode
+{
+    // Declare OpMode members.
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private static final double ACCELERATION = 0.25;
+    private static final double MAX_SPEED = 0.5;
+    private DcMotorEx shooter;
+    private Servo flicker;
+    static double x = 0;
+    private double shooterTicksPerSecond = 1000;
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initializing");
+
+        // Initialize the hardware variables. Note that the strings used here must correspond
+        // to the names assigned during the robot configuration step on the DS or RC devices.
+        shooter = hardwareMap.get(DcMotorEx.class, "shooter");//port 0
+        flicker = hardwareMap.get(Servo.class, "flicker");
+        shooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        shooter.setDirection(DcMotor.Direction.FORWARD);
+        telemetry.addData("TeleOp", "Initialized");
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+        if (gamepad1.dpad_down){
+            flicker.setPosition(0.4);
+            telemetry.addData("Flicker Status: ", " Was pressed");
+        }
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+
+        runtime.reset();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        if (gamepad1.rightBumperWasPressed()){
+            shooterTicksPerSecond += 100;
+        } else if (gamepad1.leftBumperWasPressed()) {
+            shooterTicksPerSecond -= 100;
+        } else if (gamepad1.dpadRightWasPressed()) {
+            shooterTicksPerSecond += 10;
+        } else if (gamepad1.dpadLeftWasPressed()) {
+            shooterTicksPerSecond -=10;
+        }
+        // Send calculated power to wheels
+        shooter.setPower(MAX_SPEED);
+        if (gamepad1.aWasPressed()){
+            flicker.setPosition(0.5);
+            telemetry.addData("Flicker Status:", " Was pressed");
+        }
+        if (gamepad1.dpad_down){
+            x = 0.5;
+            telemetry.addData("Flicker Status: ", " Was pressed");
+        }
+        if (gamepad1.dpad_up){
+            x = 0;
+            telemetry.addData("Flicker Status: ", "Was pressed");
+        }
+    flicker.setPosition(x);
+
+
+        // Show the elapsed game time and wheel power.
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("Ticks Per Second", shooterTicksPerSecond);
+        telemetry.addData("Controls", "Right Bumper (+100)\nright D pad (+10)\nleft bumper (-100)\nleft D pad (-10)");
+        telemetry.update();
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
+}
