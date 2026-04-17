7b762a571603f628320d0a7baafee67aed078feb
ghelanin27 <GhellaniN27@greenhill.org>
2026-04-17 10:03:45 -0500

    Ts should work (now it should work)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ShooterTestThingy/SahithShooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ShooterTestThingy/SahithShooterTest.java
@@ -75,6 +75,29 @@ public class SahithShooterTest extends OpMode
     private double leftBackPower = 0;
     private double rightBackPower = 0;
 
+    double DESIRED_DISTANCE = 30.0; //  this is how close the camera should get to the target (inches)
+    double DESIRED_YAW = 0;
+    double DESIRED_HEADING = 0;
+
+    //  Set the GAIN constants to control the relationship between the measured position error, and how much power is
+    //  applied to the drive motors to correct the error.
+    //  Drive = Error * Gain    Make these values smaller for smoother control, or larger for a more aggressive response.
+    final double SPEED_GAIN  =  0.02  ;   //  Forward Speed Control "Gain". eg: Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
+    final double STRAFE_GAIN =  0.0275;   //  Strafe Speed Control "Gain".  eg: Ramp up to 25% power at a 25 degree Yaw error.   (0.25 / 25.0)
+    final double TURN_GAIN   =  0.01  ;   //  Turn Control "Gain".  eg: Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
+
+    final double MAX_AUTO_SPEED = 1.0;   //  Clip the approach speed to this max value (adjust for your robot)
+    final double MAX_AUTO_STRAFE= 1.0;   //  Clip the approach speed to this max value (adjust for your robot)
+    final double MAX_AUTO_TURN  = 0.75;   //  Clip the turn speed to this max value (adjust for your robot)
+
+    private DcMotor frontLeft   = null;  //  Used to control the left front drive wheel
+    private DcMotor frontRight  = null;  //  Used to control the right front drive wheel
+    private DcMotor backLeft    = null;  //  Used to control the left back drive wheel
+    private DcMotor backRight   = null;  //  Used to control the right back drive wheel
+
+    private static final boolean USE_WEBCAM = true;  // Set true to use a webcam, or false for a phone camera
+    private static final int DESIRED_TAG_ID = 24;     // Choose the tag you want to approach or set to -1 for ANY tag.
+
     private double accelerate(double currentPower, double targetPower, double acceleration){
         if (currentPower < targetPower) {
             return Math.min(currentPower + acceleration, targetPower);
