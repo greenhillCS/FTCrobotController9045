b03dea92eab6f217f14edd3ddec18d4fa1441452
ghelanin27 <GhellaniN27@greenhill.org>
2026-04-17 10:23:54 -0500

    Ts should work (now it should work)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ShooterTestThingy/SahithShooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ShooterTestThingy/SahithShooterTest.java
@@ -310,7 +310,31 @@ public class SahithShooterTest extends OpMode
         }
     }
 
+    public void moveRobot(double x, double y, double yaw) {
+        // Calculate wheel powers.
+        double leftFrontPower    =  x -y -yaw;
+        double rightFrontPower   =  x +y +yaw;
+        double leftBackPower     =  x +y -yaw;
+        double rightBackPower    =  x -y +yaw;
 
+        // Normalize wheel powers to be less than 1.0
+        double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+        max = Math.max(max, Math.abs(leftBackPower));
+        max = Math.max(max, Math.abs(rightBackPower));
+
+        if (max > 1.0) {
+            leftFrontPower /= max;
+            rightFrontPower /= max;
+            leftBackPower /= max;
+            rightBackPower /= max;
+        }
+
+        // Send powers to the wheels.
+        frontLeft.setPower(leftFrontPower);
+        frontRight.setPower(rightFrontPower);
+        backLeft.setPower(leftBackPower);
+        backRight.setPower(rightBackPower);
+    }
 
     @Override
     public void init() {
