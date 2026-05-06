6f7f920995e104fb688928127922d9d5c493acb0
sewakj27 <sewakj27@greenhill.org>
2026-05-06 15:51:57 -0500

    Improved ATRunner Autons to be more consistent and work on both sides

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autons/ATRunner/ATRunnerTestBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autons/ATRunner/ATRunnerTestBlue.java
@@ -9,13 +9,13 @@ import org.firstinspires.ftc.teamcode.Tools.ATRunner.LauncherToolClass;
 import org.firstinspires.ftc.teamcode.Tools.ATRunner.TurretLimelight;
 import org.firstinspires.ftc.teamcode.Tools.ATRunner.v;
 
-@Autonomous(name="ATRunner Test", group="ATRunner")
+@Autonomous(name="ATRunner Test Blue", group="ATRunner")
 
-public class ATRunnerTest extends OpMode {
+public class ATRunnerTestBlue extends OpMode {
     // Declare OpMode members.
 
     private ElapsedTime runtime = new ElapsedTime();
-    private ATRunner atr;
+    private ATRunnerBasic atr;
     private TurretLimelight turret;
     private LauncherToolClass launcher;
     private IntakeToolClass intake;
@@ -31,12 +31,18 @@ public class ATRunnerTest extends OpMode {
     public void init() {
         telemetry.addData("Status", "Initializing");
 
+        atr = new ATRunnerBasic(hardwareMap, telemetry, gamepad1);
+        atr.addPoint(launchDist, launchYaw * m, 0 * m, id, 2.5, 1);//0
+        atr.addPoint(launchDist, launchYaw * m, 0 * m, id, launchWait, 1);//1
+        atr.addPoint(110, -48 * m, 0 * m, id, 0, 1);//2
+        atr.addPoint(80, -48 * m, 0 * m, id, 0, 1);//3
+        atr.addPoint(launchDist, launchYaw * m, 0 * m, id, 0, 1);//4
+        atr.addPoint(launchDist, launchYaw * m, 0 * m, id, launchWait, 1);//5
+        atr.addPoint(launchDist, -40 * m, 0 * m, id, 0, 1);//6
+
         turret = new TurretLimelight(hardwareMap, telemetry, gamepad1);
         launcher = new LauncherToolClass(hardwareMap, telemetry, gamepad1);
         intake = new IntakeToolClass(hardwareMap, telemetry, gamepad1);
-
-        atr = new ATRunner(hardwareMap, telemetry, gamepad1, turret);
-        atr.addPoint(100, 100, 100, 24);
 
         telemetry.addData("Status", "Initialized");
     }
@@ -55,6 +61,7 @@ public class ATRunnerTest extends OpMode {
     @Override
     public void start() {
         runtime.reset();
+        atr.init();
         intake.setState(v.STATE.IN);
         launcher.setState(v.STATE.OUT);
         turret.locked = true;
@@ -67,9 +74,35 @@ public class ATRunnerTest extends OpMode {
     public void loop() {
 //        turret.update();
         atr.update();
-//        launcher.update();
-//        intake.update();
+        launcher.update();
+        intake.update();
         turret.update();
+        if(atr.state.equals(ATRunnerBasic.STATE.WAITING)){
+            switch(atr.pointIndex){
+                case 0:
+                    break;
+                case 1, 5:
+                    launcher.launch = true;
+                    break;
+                case 2, 3, 4:
+                    launcher.launch = false;
+                    break;
+                case 6:
+                    launcher.launch = false;
+                    intake.setState(v.STATE.STOP);
+                    launcher.setState(v.STATE.STOP);
+            }
+        }else if(atr.state.equals(ATRunnerBasic.STATE.MOVING)){
+            switch(atr.pointIndex){
+                case 2, 3, 4:
+                    launcher.launch = false;
+                    break;
+                case 6:
+                    launcher.launch = false;
+                    intake.setState(v.STATE.STOP);
+                    launcher.setState(v.STATE.STOP);
+            }
+        }
         telemetry.addData("Status", "Run Time: " + runtime.toString());
     }
 
